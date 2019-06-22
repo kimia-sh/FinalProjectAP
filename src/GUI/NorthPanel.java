40eff7bfdaffe8cabb4410d36d901aa9dddb0808
9731030 <9731030@ceit.local>
2019-06-22 23:00:46 +0430

    adding North panel class

--- src/GUI/NorthPanel.java
+++ src/GUI/NorthPanel.java
@@ -1,5 +1,20 @@
 package GUI;
 
-public class NorthPanel
-{
+import javax.swing.*;
+import java.awt.*;
+
+public class NorthPanel {
+    public static void main(String args[]) {
+        JFrame f = new JFrame();
+        f.getContentPane().setLayout(new BorderLayout());
+        JTextArea area = new JTextArea(1, 20);
+        JPanel jp = new JPanel();
+        jp.setLayout(new BorderLayout());
+        JButton search = new JButton("Search");
+        jp.add(search, BorderLayout.WEST);
+        jp.add(area, BorderLayout.CENTER);
+        f.setSize(300, 300);
+        f.getContentPane().add(jp, BorderLayout.NORTH);
+        f.setVisible(true);
+    }
 }
