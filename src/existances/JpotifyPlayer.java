10bb1875097941e6b89855c52888bd33a540dfd0
kimia-sh <sh.kimia79@gmail.com>
2019-06-24 00:24:42 +0430

    adding JpotifyPlayer class that play a music

---
+++ src/existances/JpotifyPlayer.java
@@ -0,0 +1,91 @@
+package existances;
+
+
+import javazoom.jl.decoder.JavaLayerException;
+import javazoom.jl.player.advanced.AdvancedPlayer;
+
+public class JpotifyPlayer extends Thread {
+    Music music;
+    AdvancedPlayer player;
+    Boolean isPause;
+    public JpotifyPlayer(Music music){
+        this.music = music;
+        try {
+            player=new AdvancedPlayer(music.getMusicStream());
+        } catch (JavaLayerException e) {
+            e.printStackTrace();
+            System.out.println("1");
+        }
+
+
+
+        isPause=false;
+
+    }
+
+    @Override
+    public void run() {
+        boolean playing=true;
+        while(playing){
+            if((isPause)){
+                synchronized (player) {
+                    try {
+                        player.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                        System.out.println("3");
+                    }
+                }
+
+            }
+            else {
+                try {
+                    playing = player.play(1);
+                } catch (JavaLayerException e) {
+                    e.printStackTrace();
+                    System.out.println("2");
+                    playing = false;
+                }
+            }
+        }
+
+    }
+    public void mp3Pause(){
+        this.isPause=true;
+    }
+    public void mp3Resume(){
+        this.isPause=false;
+        synchronized (player){
+            player.notifyAll();
+        }
+
+    }
+    public void mp3Play(){
+        this.start();
+    }
+    public void seekTo(int frame){
+        synchronized (player){
+            player.close();
+            try {
+                player=new AdvancedPlayer(music.getMusicStream());
+            } catch (JavaLayerException e) {
+                e.printStackTrace();
+                System.out.println("6");
+
+            }
+            try {
+                player.play(frame,frame+1);
+            } catch (JavaLayerException e) {
+                e.printStackTrace();
+                System.out.println("7");
+            }
+        }
+    }
+    public void close(){
+        this.suspend();
+    }
+
+
+
+
+}
