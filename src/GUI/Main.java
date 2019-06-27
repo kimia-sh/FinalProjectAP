7db4d0857ee8a0cec0e655cc8d35b234e7385b31
9731030 <9731030@ceit.local>
2019-06-27 19:54:04 +0430

    complete main with new GUI

--- src/GUI/Main.java
+++ src/GUI/Main.java
@@ -1,8 +1,12 @@
 package GUI;
 
+import existances.Music;
+import existances.PlayList;
+
 public class Main {
 
     public static void main(String[] args) {
+
         GUI gui = new GUI();
     }
 }
