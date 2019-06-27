38beb1679fe0293e72f6e5497f75c3d7fea344db
9731030 <9731030@ceit.local>
2019-06-27 19:55:18 +0430

    deleting playList panel

--- src/GUI/PlaylistPanel.java
+++ src/GUI/PlaylistPanel.java
@@ -1,12 +1,19 @@
 package GUI;
 
+import existances.Music;
+import existances.PlayList;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
-public class PlaylistPanel extends JPanel{
+public class PlaylistPanel extends JPanel implements ActionListener {
       JLabel playlists;
       JPanel showPlaylists;
+      PlayList playList;
+    JButton add_playlist_button;
       public PlaylistPanel(){
            setLayout(new BorderLayout());
           playlists=new JLabel("playlists");
@@ -18,12 +25,31 @@ public class PlaylistPanel extends JPanel{
           scrollableArea.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
           add(playlists,BorderLayout.NORTH);
           add(scrollableArea,BorderLayout.CENTER);
-          JButton button = new JButton("add PlayLists");
-          add(button,BorderLayout.SOUTH);
-
+          add_playlist_button = new JButton("add PlayLists");
+          add(add_playlist_button,BorderLayout.SOUTH);
+          add_playlist_button.addActionListener(this);
 
 
       }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource().equals(add_playlist_button))
+        {
+            playList = new PlayList("Reyhaneh");
 
+            Music music1 = new Music("E://1.mp3","1");
+            Music music2 = new Music("E://2.mp3","2");
+            Music music3 = new Music("E://3.mp3","3");
+            Music music4 = new Music("E://4.mp3","4");
+            playList.add(music1);
+            playList.add(music2);
+            playList.add(music3);
+            playList.add(music4);
+
+        }
+    }
+    PlayList getPlayList(){
+        return playList;
+    }
 
 }
