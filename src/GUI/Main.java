73078c9a5d5cb91cde40e6018f7fdf15ba666d4d
kimia-sh <sh.kimia79@gmail.com>
2019-06-28 16:29:18 +0430

    changing Main to show first page

--- src/GUI/Main.java
+++ src/GUI/Main.java
@@ -3,10 +3,20 @@ package GUI;
 import existances.Music;
 import existances.PlayList;
 
+import java.io.File;
+
 public class Main {
 
     public static void main(String[] args) {
+        File information = new File("E://ap/Project/FinalProjectAP/File/information.txt");
 
-        GUI gui = new GUI();
+        if (!information.exists()) {
+            FirstPage firstPage = new FirstPage();
+        }
+
+        else {
+            GUI gui=new GUI();
+            }
+
+        }
     }
-}
